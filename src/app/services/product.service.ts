029f9eeb17f76dcfa24b834dc0f60ea48fe2e14b
Yahia <mohamedyahia207545@gmail.com>
2023-07-21 21:58:30 +0300

    crud

---
+++ src/app/services/product.service.ts
@@ -0,0 +1,42 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, Subject } from 'rxjs';
+import { IProduct } from '../models/iProudct';
+import { environment } from 'src/environments/environment.development';
+
+@Injectable({
+  providedIn: 'any',
+})
+export class ProductService {
+  private baseURL: string = environment.baseURL + '/products';
+  constructor(private http: HttpClient) {}
+
+  addProductList(product: IProduct): Observable<IProduct> {
+    return this.http.post<IProduct>(this.baseURL, product);
+  }
+
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.baseURL);
+  }
+
+  getOneProduct(id: number): Observable<IProduct> {
+    return this.http.get<IProduct>(`${this.baseURL}/${id}`);
+  }
+
+  getCategories(): Observable<string[]> {
+    return this.http.get<string[]>(`${this.baseURL}/categories`);
+  }
+
+  editingSubject: Subject<IProduct> = new Subject<IProduct>();
+  editProudct(product: IProduct) {
+    this.editingSubject.next(product);
+  }
+
+  updateProduct(product: IProduct): Observable<IProduct> {
+    return this.http.patch<IProduct>(`${this.baseURL}/${product.id}`, product);
+  }
+
+  deleteProduct(id: number): Observable<IProduct> {
+    return this.http.delete<IProduct>(`${this.baseURL}/${id}`);
+  }
+}
